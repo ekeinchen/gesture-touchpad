2f12691592af869d07695fc56fed2a18adb3e212
ekeinchen <107114079+ekeinchen@users.noreply.github.com>
2025-06-12 16:58:43 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = "gesture-pwa-v1";
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
